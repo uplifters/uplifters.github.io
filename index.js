fbf84fc615fefd7c796f84c6d633994d7195412a
marko bogdanovic <okram@protonmail.ch>
2021-06-20 20:12:16 +0200

    improve page, add proper responsiveness

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-
-/*
-
-    - introduction
-    - screenshots
-
-    - about me
-    - patreon
-    - privacy stuff
-
-*/
 // https://github.com/uplifters/uplifters.github.io
-    // git remote add origin https://github.com/uplifters/uplifters.github.io.git
-// https://ritwickdey.github.io/vscode-live-server/
 
 // https://www.css-gradient.com/
 // https://cssgradient.io/gradient-backgrounds/
@@ -32,15 +19,8 @@ const img = document.getElementById('carousel');
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
 
-// Images are from unsplash
-// let pictures = [
-//     'https://images.unsplash.com/photo-1537000092872-06bbf7b64f60?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=14d2fe1244b43a1841569da918066fc4&auto=format&fit=crop&w=1050&q=80', 
-//     'https://images.unsplash.com/photo-1537005081207-04f90e3ba640?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ffb71f2a2843e802e238c5ff8e4bbb8c&auto=format&fit=crop&w=764&q=80', 
-//     'https://images.unsplash.com/photo-1536873602512-8e88cc8398b1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=60a351868d0839e686c8c5a286265f8d&auto=format&fit=crop&w=1050&q=80'
-// ];
 
 const pictures = Array.from(document.querySelectorAll('img.slideshow-img')).map(img => img.src) // picURLs
-
 img.src = pictures[0];
 let position = 0;
 
@@ -74,7 +54,6 @@ rightBtn.addEventListener("click", moveRight);
 leftBtn.addEventListener("click", moveLeft);
 
 function changeImage(){
-    // log('change')
 
     position++;
     if (position >= pictures.length) position = 0
--- notes.md
+++ notes.md
@@ -2,3 +2,7 @@
 https://www.privacypolicies.com/blog/privacy-policy-template/
 
 https://app.privacypolicies.com/download/0f729d36-3d65-448a-b6ec-9884ac3add37
+
+
+https://cssgradient.io/gradient-backgrounds/
+https://freefrontend.com/javascript-carousels/
